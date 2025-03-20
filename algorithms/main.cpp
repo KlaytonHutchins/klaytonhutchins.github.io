d59670997cc64d342bca7dbf0813148824ad83c8
Klayton Hutchins <klaytonhutchins@gmail.com>
2025-03-19 21:04:20 -0700

    add more notes

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -2,18 +2,24 @@
 #include <fstream>
 #include <sstream>
 #include <curl/curl.h>
-#include "Nodes.h"
+#include "Case.h"
+#include "Node.h"
 #include "DoublyLL.cpp"
 
 using namespace std;
 
+// int currYear = idk, system . get year ?
 int theYear = 2023;
 
 string toLower(string str);
 string sanitizeFileName(string str);
 size_t WriteToFile(void* ptr, size_t size, size_t nmemb, FILE* stream);
-void downloadPDFs(DoublyLL<CaseNode>* caseList);
-bool downloadPDF(CaseNode* curr, string& savePath);
+void downloadPDFs(DoublyLL<Node<Case>>* caseList);
+bool downloadPDF(Node<Case>* curr, string& savePath);
+size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp);
+void scrape(string url, string fileName);
+DoublyLL<Node<Case>>* generateListFromHtml(string termYear);
+void writeFileFromList(DoublyLL<Node<Case>>* casesForTermYear);
 
 string toLower(string str) {
         for (size_t i = 0; i < str.length(); i++) {
@@ -35,24 +41,24 @@ string sanitizeFileName(string str) {
 size_t WriteToFile(void* ptr, size_t size, size_t nmemb, FILE* stream) {
         return fwrite(ptr, size, nmemb, stream);
 }
-
-void downloadPDFs(DoublyLL<CaseNode>* caseList) {
+/*
+void downloadPDFs(DoublyLL<Node<Case>>* caseList) {
         string saveFolder = "../scotus/" + to_string(theYear) + "/resources/";
         system(("mkdir -p " + saveFolder).c_str());
-        CaseNode* curr = caseList->getHead();
+        Node<Case>* curr = caseList->getHead();
         while (curr) {
-                string fileName = saveFolder + sanitizeFileName(curr->getDocketNum()) + ".pdf";
-                cout << "Downloading: " << curr->getUrl() << " -> " << fileName << endl;
+                string fileName = saveFolder + sanitizeFileName(curr->getData()->getDocketNum()) + ".pdf";
+                cout << "Downloading: " << curr->getData()->getUrl() << " -> " << fileName << endl;
                 if (downloadPDF(curr, fileName)) {
                         cout << "Saved: " << fileName << endl;
                 } else {
-                        cerr << "Download failed: " << curr->getUrl() << endl;
+                        cerr << "Download failed: " << curr->getData()->getUrl() << endl;
                 }
                 curr = curr->getNext();
         }
 }
 
-bool downloadPDF(CaseNode* curr, string& savePath) {
+bool downloadPDF(Node<Case>* curr, string& savePath) {
         CURL* curl = curl_easy_init();
         if (!curl) {
                 cerr << "Failed to initialize curl" << endl;
@@ -64,14 +70,14 @@ bool downloadPDF(CaseNode* curr, string& savePath) {
                 curl_easy_cleanup(curl);
                 return false;
         }
-        curl_easy_setopt(curl, CURLOPT_URL, curr->getUrl().c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, curr->getData()->getUrl().c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteToFile);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // Follow redirects
 
         CURLcode res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
-                cerr << "Failed to download: " << curr->getUrl() << " | Error: " << curl_easy_strerror(res) << endl;
+                cerr << "Failed to download: " << curr->getData()->getUrl() << " | Error: " << curl_easy_strerror(res) << endl;
                 fclose(file);
                 remove(savePath.c_str()); // Remove incomplete file
                 curl_easy_cleanup(curl);
@@ -80,64 +86,92 @@ bool downloadPDF(CaseNode* curr, string& savePath) {
         fclose(file);
         curl_easy_cleanup(curl);
         return true;
+}*/
+
+size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+        ofstream* outFile = static_cast<ofstream*>(userp);
+        outFile->write(static_cast<char*>(contents), size * nmemb);
+        return size * nmemb;
+}
+
+void scrape(string url, string outputFilename) {
+        CURL* curl;
+        CURLcode res;
+        ofstream outFile(outputFilename);
+
+        if (!outFile.is_open()) {
+                cerr << "Error opening output file!" << endl;
+        }
+
+        curl = curl_easy_init();
+        if (curl) {
+                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &outFile);
+
+                res = curl_easy_perform(curl);
+
+                if (res != CURLE_OK) {
+                        cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+                } else {
+                        cout << "Data successfully fetched and saved to " << outputFilename << endl;
+                }
+
+                curl_easy_cleanup(curl);
+        } else {
+                cerr << "Failed to initialize curl." << endl;
+        }
+
+        outFile.close();
+}
+
+DoublyLL<Node<Case>>* generateListFromHtml(string termYear) {
+	// reopen temp html file
+	ifstream inFile("tempHtml.txt");
+	string str;
+	// scan through line-by-line
+	DoublyLL<Node<Case>>* cases = new DoublyLL<Node<Case>>();
+	while (getline(inFile, str, '\n')) {
+		if (str == "<tr>") {
+			//Node<Case>* newNode = new Node<Case>();
+			getline(inFile, str, '\n');
+			// newNode->setOrdNum(str);
+			getline(inFile, str, '\n');
+			// date = convertDate(str);
+			getline(inFile, str, '\n');
+			// caseNum = str;
+			getline(inFile, str, '\n');
+			// url = first part
+			// caseName = second part;
+			getline(inFile, str, '\n');
+			// trash
+			getline(inFile, str, '\n');
+			// trash
+			// myList->insert ( , back, front)?
+		}
+		/* EXAMPLE of 1 case
+<tr>
+	<td style="text-align: center;">17</td>
+	<td style="text-align: center;">3/05/25</td>
+	<td style="text-align: center; white-space: nowrap;">23-713</td>
+	<td><a href="/opinions/24pdf/23-713_jifl.pdf" target="_blank" title="The Department of Veterans Affairs’ determination that the evidence regarding a service-related disability claim is in “approximate balance” pursuant to the “benefit-of-the-doubt rule,” 38 U. S. C. §5107(b), is a predominantly factual determination reviewed only for clear error.">Bufkin v. Collins</a></td>
+	<td style="text-align: center;">T</td>
+	<td style="text-align: center;"><span style="white-space:nowrap;">604/1</span></td>
+</tr>
+*/
+	}
+	//return new DoublyLL<CaseNode>();
+	return cases;
+	// if contains <li> write, otherwise skip
+}
+
+void writeFileFromList(DoublyLL<Node<Case>>* casesForTermYear) {
+	
 }
 
 int main() {
-//        DoublyLL<CaseNode> myList = DoublyLL<CaseNode>();
-//        myList.insertBack(new CaseNode("32", "03/23/2023", "24-975", "Klayton v. Selena", "www.website.com"));
-//        myList.insertBack(new CaseNode("33", "03/24/2023", "24-976", "Klayton v. Quiche", "www.websites.com"));
-//        myList.insertBack(new CaseNode("34", "03/25/2023", "24-977", "Klayton v. Jack", "www.web.com"));
-//        myList.insertBack(new CaseNode("35", "03/26/2023", "24-978", "Jack v. Quiche", "www.site.com"));
-//        cout << myList.getHead()->getNext()->getDocketNum() << endl;
-
-
-        ifstream inFile;
-//        inFile.open(inFileName);
-        inFile.open("2023input.txt");
-//        string ordNum, date, docketNum, caseName, url;
-        DoublyLL<CaseNode>* entryList = new DoublyLL<CaseNode>();
-        string str;
-        while (getline(inFile, str, '\n')) {
-                  //CaseNode::CaseNode(sstream& inputStr)
-                  stringstream sstr(str);
-                  entryList->insertBack(new CaseNode(sstr));
-//                getline(inFile, date, '\t');
-//                getline(inFile, docketNum, '\t');
-//                getline(inFile, caseName, '\t');
-//                getline(inFile, url, '\n');
-//                cout << "Reading " << docketNum << endl;
-//                entryList->insertBack(ordNum, date, docketNum, caseName, url);
-        }
-        
-        inFile.close();/*
-        ofstream outFile;
-        outFile.open("../scotus/" + to_string(yearToAccess) + "/README.md");
-        cout << "Building " << to_string(yearToAccess) << " README" << endl;
-        entryList->buildYearReadMe(outFile);
-        outFile.close();*/
-
-        CaseNode* curr = entryList->getHead();
-        //string pageName;
-//        cout << curr->getOrdNum() << " " << curr->getDate();
-//        cout << " " << curr->getDocketNum() << " " << curr->getCaseName();
-//        cout << " " << curr->getUrl() << endl;
-        cout << "size: " << entryList->getSize() << endl;
-        int count = 2;
-        while (curr && count > 0) {
-                cout << curr->getOrdNum() << endl;
-                cout << curr->getDate() << endl;
-                cout << curr->getDocketNum() << endl;
-                cout << curr->getCaseName() << endl;
-                cout << curr->getUrl() << endl;
-//                pageName = "../scotus/" + to_string(yearToAccess) + "/" + sanitizeFileName(curr->getDocketNum()) + ".md";
-//                outFile.open(pageName);
-//                curr->buildCasePage(outFile);
-//                outFile.close();
-                curr = curr->getNext();
-                count--;
-        }
-//        downloadPDFs(entryList);
-        delete entryList;
-        return 0;
-
+	string url = "https://www.supremecourt.gov/opinions/slipopinion/" + to_string(theYear - 2000);
+        string outputFilename = "tempHtml.txt";
+	scrape(url, outputFilename);
 }
+
